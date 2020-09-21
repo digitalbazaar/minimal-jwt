1fbec30cc8ad6b10cad10169cf4d11fea117da24
Ganesh Annan <gannan@digitalbazaar.com>
2020-09-21 11:01:46 -0400

    Simplify logic.

--- util.js
+++ util.js
@@ -3,11 +3,27 @@
  */
 // Node.js TextDecoder/TextEncoder
 import {TextDecoder, TextEncoder} from 'util';
+import base64url from 'base64url-universal';
 
 export function createJwsSigningInput({encodedHeader, encodedPayload}) {
   return new TextEncoder().encode(encodedHeader + '.' + encodedPayload);
 }
 
-export function bytesToString(bytes) {
+export function b64UrlEncodedStringToObject({str, name}) {
+  let obj;
+  try {
+    obj = JSON.parse(bytesToString(base64url.decode(str)));
+  } catch(e) {
+    throw new Error(`Could not parse ${name}; ` + e);
+  }
+
+  if(!(obj && typeof obj === 'object')) {
+    throw new Error(`Invalid ${name}.`);
+  }
+
+  return obj;
+}
+
+function bytesToString(bytes) {
   return new TextDecoder('utf-8').decode(bytes);
 }
