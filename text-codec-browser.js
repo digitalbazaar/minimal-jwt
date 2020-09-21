2e71ff4af2fe767ce2ae20986f4dbf8feda06bc1
Ganesh Annan <gannan@digitalbazaar.com>
2020-09-21 12:57:10 -0400

    Isolate text encoder/decoder in their isomorphic module.

---
+++ text-codec-browser.js
@@ -0,0 +1,7 @@
+/*!
+ * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ */
+// browser TextDecoder/TextEncoder
+/* eslint-env browser */
+const {TextDecoder, TextEncoder} = self;
+export {TextDecoder, TextEncoder};
